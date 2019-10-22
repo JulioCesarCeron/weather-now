229b5cb81d10fb564ef8950697e2367859d8f512
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-10-22 07:12:38 -0300

    config styles and add a shadow to header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className={styles.container}>
       <Header />
+      <div className={styles['wrapper-weather']}>
+      <hr/>
+
+      </div>
     </div>
   );
 }
