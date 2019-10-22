6ee394bed3216a5dce5c381661725279331c5520
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-10-21 22:55:38 -0300

    initial structure

---
+++ src/components/MainWeatherCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainWeatherCard = () => {
+  return (
+    <div>
+      MainWeatherCard
+    </div>
+  );
+};
+
+export default MainWeatherCard;
---
+++ src/components/SecondaryWeatherCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SecondaryWeatherCard = () => {
+  return (
+    <div>
+      SecondaryWeatherCard
+    </div>
+  );
+};
+
+export default SecondaryWeatherCard;
