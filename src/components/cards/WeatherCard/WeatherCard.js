9d0f467fb34d1ee47a6b9e8551f038907470033f
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-10-24 20:43:27 -0300

    add setInterval

--- src/components/cards/WeatherCard/WeatherCard.js
+++ src/components/cards/WeatherCard/WeatherCard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import styles from './WeatherCard.module.css'
-import { getWeater } from '../../../config/api'
+import { getWeater, getLocalData, setLocalData} from '../../../config/api'
 import { WEATHER_API } from '../../../config/index'
 import loader from '../../../assets/loader.svg';
 
@@ -17,24 +17,89 @@ const WeatherCard = ({ main, city, country }) => {
     pressure: ''
   });
 
+  const getDiffBetweenCurrentAndLocalDate = (savedCityInfo) => {
+    const localDate = new Date(savedCityInfo.date);
+    const currentDate = new Date();
+    const diffMinutes = (((currentDate.getTime() - localDate.getTime()) / 1000) / 60).toFixed();
+    console.log('diffMinutes', diffMinutes);
+    return diffMinutes;
+  }
+
   useEffect(() => {
-    const url = WEATHER_API(city, country);
-    setLoading(true);
-    getWeater(url).then(res => {
-      setLoading(false);
+
+    const setComponentData = (receivedData) => {
+      console.log('receivedData', receivedData);
       if (main) {
-        setWeather({
-          temp: kelvinToCelcius(res.data.main.temp),
-          humidity: res.data.main.humidity,
-          pressure: res.data.main.pressure
-        });
+        setWeather({ ...receivedData });
       } else {
         setWeather({
-          temp: kelvinToCelcius(res.data.main.temp),
+          temp: receivedData.temp,
         })
       }
-    });
+    }
+
+    const onGetDataFromWeather = () => {
+      setLoading(true);
+      const url = WEATHER_API(city, country);
+      getWeater(url).then(res => {
+        setLoading(false);
+
+        const receivedData = {
+          temp: kelvinToCelcius(res.data.main.temp),
+          humidity: res.data.main.humidity,
+          pressure: res.data.main.pressure,
+          date: new Date(),
+        }
+
+        setComponentData(receivedData)
+
+        onSetLocalData({
+          city,
+          country,
+          weather: receivedData,
+        })
+      });
+    }
+
+    const verifyLastUpdate = (savedCityInfo) => {
+      const localStorageData = JSON.parse(savedCityInfo);
+      const minutes = getDiffBetweenCurrentAndLocalDate(localStorageData)
+      if (minutes >= 10) {
+        onGetDataFromWeather();
+      } else {
+        setComponentData(localStorageData);
+      }
+    }
+    
+    const onGetLocalData = async() => {
+      const localSavedCityInfo = await getLocalData({city, country});
+      if (!localSavedCityInfo) {
+        onGetDataFromWeather();
+      } else {
+        verifyLastUpdate(localSavedCityInfo);
+      }
+    }
+
+    const onSetLocalData = async (data) => {
+      await setLocalData(data);
+    }
+
+    onGetLocalData();
+    setInterval(() => onGetLocalData(), 50000);
+    
   }, [city, country, main])
+
+  const getColorTemperature = () => {
+    if (weather.temp <= 5) {
+      return 'cold';
+    }
+
+    if (weather.temp > 5 && weather.temp <= 25) {
+      return 'medium';
+    }
+
+    return 'hot'
+  }
 
   return (
     <div className={`${styles.card} ${main && styles['card-order']}`}>
@@ -45,7 +110,7 @@ const WeatherCard = ({ main, city, country }) => {
       {
         loading
           ? <img src={loader} className={styles.logo} alt="logo" />
-          : <p className={`${styles.temperature} ${styles.medium}`}>{weather.temp}º</p>
+          : <p className={`${styles.temperature} ${styles[getColorTemperature()]}`}>{weather.temp}º</p>
       }
       </div>
       <div className={ loading ? styles['footer-loading'] : styles.footer}>
--- src/config/api.js
+++ src/config/api.js
@@ -4,11 +4,10 @@ export const getWeater = (api) => {
   return axios.get(api)
 }
 
-export const getLocalData = async (data) => {
-  let localData = await localStorage.getItem((`${data.city}${data.country}`).toLowerCase());
-  return localData;
+export const getLocalData = (data) => {
+  return localStorage.getItem((`${data.city}${data.country}`).toLowerCase());
 }
 
-export const setLocalData = async (data) => {
-  await localStorage.setItem((`${data.city}${data.country}`).toLowerCase(), JSON.stringify(data.weather));
+export const setLocalData = (data) => {
+  localStorage.setItem((`${data.city}${data.country}`).toLowerCase(), JSON.stringify(data.weather));
 }
