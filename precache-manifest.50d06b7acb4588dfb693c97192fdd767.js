d1d2dd414c9890c55225365d4f24fa131feeb50e
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-10-25 20:27:41 -0300

    Updates

---
+++ precache-manifest.50d06b7acb4588dfb693c97192fdd767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c0d3864f30777e17cd83bb0fdd418d",
+    "url": "/JulioCesarCeron/weather-now/index.html"
+  },
+  {
+    "revision": "64f583e1347df5b110af",
+    "url": "/JulioCesarCeron/weather-now/static/css/main.65ad36f4.chunk.css"
+  },
+  {
+    "revision": "a12d9b5a7654856a4406",
+    "url": "/JulioCesarCeron/weather-now/static/js/2.2aa88400.chunk.js"
+  },
+  {
+    "revision": "64f583e1347df5b110af",
+    "url": "/JulioCesarCeron/weather-now/static/js/main.9231c8b9.chunk.js"
+  },
+  {
+    "revision": "794d252ee2287b3ad405",
+    "url": "/JulioCesarCeron/weather-now/static/js/runtime-main.702d8dc7.js"
+  },
+  {
+    "revision": "d542c686fe5742a713e3f0038a3a7a8e",
+    "url": "/JulioCesarCeron/weather-now/static/media/loader.d542c686.svg"
+  },
+  {
+    "revision": "45377fd606e81a3caefe9f57e624931f",
+    "url": "/JulioCesarCeron/weather-now/static/media/logo.45377fd6.svg"
+  }
+]);
