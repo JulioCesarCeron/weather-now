2f41ce80a4f6db6e5ca47975660381ee3e6e019a
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-10-29 00:32:31 -0300

    change url to https

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,3 +1,3 @@
 
 const API_KEY = '8561a5b7b131a9f8f6f5996bf4bb2c07';
-export const WEATHER_API = (city, country) => `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`;
+export const WEATHER_API = (city, country) => `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`;
